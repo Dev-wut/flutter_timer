924d9d2e6ceaf0884b6da9a27a6759219c978edc
worawut.je <worawut.je@summitthai.com>
2025-12-18 10:31:24 +0700

    - completed example

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,4 @@
 import 'package:flutter/material.dart';
+import 'app.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const Placeholder(),
-    );
-  }
-}
+void main() => runApp(const App());
